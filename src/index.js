7c4becdcc6209d67271d4e069d10b50d0e121f25
Your Name <you@example.com>
2024-09-29 14:55:29 +0200

    Refactor downloadRemoteFile function and implement pendingRequests queue

--- src/index.js
+++ src/index.js
@@ -18,14 +18,14 @@ let cachePath = process.env.CACHE_PATH || '/cached/';
 const app = express();
 const target = process.env.TARGET || 'http://nginx:80';
 const proxy = httpProxy.createProxyServer({ target });
-async function downloadRemoteFile(unfinishedDownloadPath, cachedDownloadPath, targetUrl){
+async function downloadRemoteFile(unfinishedDownloadPath, cachedDownloadPath, targetUrl) {
     console.log('Checking if file exists', cachedDownloadPath);
     if (fs.existsSync(unfinishedDownloadPath)) {
         fs.unlinkSync(unfinishedDownloadPath);
     }
     console.log('Creating download stream');
     let cachedDownloadStream = fs.createWriteStream(unfinishedDownloadPath);
-    let headed = await fetch(targetUrl, {method: 'HEAD'});
+    let headed = await fetch(targetUrl, { method: 'HEAD' });
     let contentLength = headed.headers.get('content-length');
     let interval = setInterval(() => {
         let stats = fs.statSync(unfinishedDownloadPath);
@@ -48,37 +48,48 @@ async function downloadRemoteFile(unfinishedDownloadPath, cachedDownloadPath, ta
     });
     fs.renameSync(unfinishedDownloadPath, cachedDownloadPath);
 };
-let downloadRemoteFileMemoized = memoize(downloadRemoteFile, {maxAge: 1000 * 60 * 60 * 1}); // 1 hours
-
-app.use(async (req, res) => {
-    if (req.url.startsWith('/stream/')) {
-        let filename = djb2(req.url);
-        let cachedPath = path.join(cachePath, filename + '.mp4');
-        if (fs.existsSync(cachedPath)) {
-            console.log('Serving from cache');
-            res.sendFile(cachedPath);
-        } else {
-            console.log('Transcoding');
-            let cachedDownloadPath = path.join(cachePath, "downloaded_" + filename + '.mkv');
-            let unfinishedDownloadPath = cachedDownloadPath + '.part';
-            console.log('Will downloading from', target + req.url);
-            if (!fs.existsSync(cachePath)) {
-                fs.mkdirSync(cachePath);
-            }
-            if (fs.existsSync(cachedDownloadPath)) {
-                console.log('Downloaded file already exists, ', cachedDownloadPath);
+let downloadRemoteFileMemoized = memoize(downloadRemoteFile, { maxAge: 1000 * 60 * 60 * 1 }); // 1 hours
+let pendingRequests = [];
+(async function () {
+    while (true) {
+        while (pendingRequests.length > 0) {
+            console.log('Pending requests', pendingRequests.length);
+            let { req, res } = pendingRequests.shift();
+            if (req.url.startsWith('/stream/')) {
+                let filename = djb2(req.url);
+                let cachedPath = path.join(cachePath, filename + '.mp4');
+                if (fs.existsSync(cachedPath)) {
+                    console.log('Serving from cache');
+                    res.sendFile(cachedPath);
+                } else {
+                    console.log('Transcoding');
+                    let cachedDownloadPath = path.join(cachePath, "downloaded_" + filename + '.mkv');
+                    let unfinishedDownloadPath = cachedDownloadPath + '.part';
+                    console.log('Will downloading from', target + req.url);
+                    if (!fs.existsSync(cachePath)) {
+                        fs.mkdirSync(cachePath);
+                    }
+                    if (fs.existsSync(cachedDownloadPath)) {
+                        console.log('Downloaded file already exists, ', cachedDownloadPath);
+                    } else {
+                        await downloadRemoteFileMemoized(unfinishedDownloadPath, cachedDownloadPath, target + req.url);
+                    }
+                    res.sendFile(cachedDownloadPath);
+                }
             } else {
-            await downloadRemoteFileMemoized(unfinishedDownloadPath, cachedDownloadPath, target + req.url);
+                if (req.url === '/health') {
+                    res.send('OK');
+                } else {
+                    proxy.web(req, res);
+                }
             }
-            res.sendFile(cachedDownloadPath);
         }
-    } else {
-        if (req.url === '/health') {
-            res.send('OK');
-        } else {
-            proxy.web(req, res);
-        }
+        await new Promise((resolve) => setTimeout(resolve, 100));
     }
+})();
+
+app.use((req, res) => {
+    pendingRequests.push({ req, res });
 });
 
 app.listen(serverPort, () => {
